f200a682666388f3cd1eda6ea1576698799deda9
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2021-12-19 10:22:08 +0530

    initial

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quam
+        voluptatum. Unde assumenda quod quis, eum sint deserunt fugit inventore
+        atque asperiores vero corporis, repellendus molestias commodi voluptates
+        blanditiis doloribus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
